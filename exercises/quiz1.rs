6630aa04a10367ca14dc5c134aaeb9a00530fc0c
Tom Bresson <tom_bresson@bose.com>
2021-07-30 17:18:37 -0400

    Commit completed exercizes

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -7,7 +7,6 @@
 // more than 40 at once, each apple only costs 1! Write a function that calculates
 // the price of an order of apples given the quantity bought. No hints this time!
 
-// I AM NOT DONE
 
 // Put your function here!
 // fn ..... {
@@ -22,4 +21,15 @@ fn verify_test() {
     assert_eq!(70, price1);
     assert_eq!(80, price2);
     assert_eq!(65, price3);
+}
+
+
+fn calculate_apple_price(qty: u32) -> u32{
+
+    if qty <= 40 {
+        2 * qty
+    }
+    else{
+        qty
+    }
 }
